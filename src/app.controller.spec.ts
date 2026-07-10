835bd9257716b2269a76813304dff75921d073d7
Jay Kim <oophunter@gmail.com>
2026-07-10 11:05:36 +0900

    문구수정

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello Jay!');
+    it('should return "Hello! This is Jay!! HaHa!"', () => {
+      expect(appController.getHello()).toBe('Hello! This is Jay!! HaHa!');
     });
   });
 });
